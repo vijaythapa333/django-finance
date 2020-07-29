9a04cb8f0f615a59e88d1bfc633ff35d3d1055da
Vijay Thapa <vijaythapa333@gmail.com>
2020-07-29 21:54:05 +0545

    Chart Added, Export to CSV and XLS also added

--- expenses/urls.py
+++ expenses/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('edit-expense/<int:id>/', views.edit_expense, name="edit-expense"),
     path('delete-expense/<id>/', views.delete_expense, name="delete-expense"),
     path('search-expenses/', csrf_exempt(views.search_expenses), name="search-expenses"),
+    path('expense-category-summary/', views.expense_category_summary, name="expense-category-summary"),
+    path('stats/', views.stats_view, name="stats"),
+    path('export-csv/', views.export_csv, name="export-csv"),
+    path('export-excel/', views.export_excel, name="export-excel"),
+    path('export-pdf/', views.export_pdf, name="export-pdf"),
 ]
--- expenses/views.py
+++ expenses/views.py
@@ -4,8 +4,17 @@ from .models import Category, Expense
 from django.contrib import messages
 from django.core.paginator import Paginator
 import json
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from userpreferences.models import UserPreference
+import datetime
+import csv
+import xlwt
+
+#importing for pdf
+from django.template.loader import render_to_string
+# from weasyprint import HTML
+import tempfile
+from django.db.models import Sum
 
 
 # Create your views here.
@@ -131,3 +140,108 @@ def search_expenses(request):
         data = expenses.values() # To pass all the values in the form of list
         
         return JsonResponse(list(data), safe=False)
+
+
+@login_required(login_url='login')
+def expense_category_summary(request):
+    todays_date = datetime.date.today()
+    six_months_ago = todays_date-datetime.timedelta(days=30*6)
+
+    expenses = Expense.objects.filter(owner=request.user, date__gte=six_months_ago, date__lte=todays_date)
+
+    final_data = {}
+
+    def get_category(expense):
+        return expense.category.id
+    category_list = list(set(map(get_category, expenses)))
+
+
+    def get_expense_category_amount(category):
+        amount = 0
+        filtered_by_category = expenses.filter(category=category)
+        for item in filtered_by_category:
+            amount += item.amount
+        return amount
+
+
+    for x in expenses:
+        for y in category_list:
+            final_data[y] = get_expense_category_amount(y)
+
+    return JsonResponse({'expense_category_data':final_data}, safe=False)
+
+
+def stats_view(request):
+    return render(request, 'expenses/stats.html')
+
+
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=Expenses'+ str(datetime.datetime.now()) +'.csv'
+
+    writer = csv.writer(response)
+    writer.writerow(['Amount', 'Description', 'Category', 'Date']) # File Header
+
+    expenses = Expense.objects.filter(owner=request.user)
+    # Data from Database
+    for expense in expenses:
+        writer.writerow([expense.amount, expense.description, expense.category.name, expense.date])
+    
+    return response
+
+
+def export_excel(requet):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Disposition'] = 'attachment; filename=Expenses'+ str(datetime.datetime.now()) +'.xls'
+
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    columns = ['Amount', 'Description', 'Category', 'Date']
+
+    # For Header
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+    
+    font_style = xlwt.XFStyle()
+
+    rows = Expense.objects.filter(owner=requet.user).values_list('amount', 'description', 'category', 'date')
+
+    for row in rows:
+        row_num += 1
+
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)
+
+    wb.save(response)
+
+    return response
+
+
+
+def export_pdf(request):
+    # response = HttpResponse(content_type='application/pdf')
+    # response['Content-Disposition'] = 'attachment; filename=Expenses'+ str(datetime.datetime.now()) +'.pdf'
+    # response['Content-Transfer-Encoding'] = 'binary'
+    # context = {
+    #     'expenses': [],
+    #     'total': 0,
+    # }
+    # html_string = render_to_string('expenses/pdf_output.html', context)
+    # html = HTML(string=html_string)
+    # result = html.write_pdf()
+
+
+    # with tempfile.NamedTemporaryFile(delete=True) as output:
+    #     output.write(result)
+    #     output.flush()
+
+    #     output = open(output.name, 'rb')
+    #     response.write(output.read())
+
+    # return response
+    pass
